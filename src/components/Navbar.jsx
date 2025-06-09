9b052c2542432f557a6ed403d11623506b9c83be
chinmaygupta0 <chinmaygupta0987@gmail.com>
2025-06-09 22:51:27 +0530

    Final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> Chinmay </span>{" "}
+            <span className="text-glow text-foreground"> Chinmay's </span>{" "}
             Portfolio
           </span>
         </a>
